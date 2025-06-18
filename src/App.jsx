c173944fcd83628ca32a1502b446c00530ac5db0
hsuyunuyn <5b1g0011@stust.edu.tw>
2025-06-18 12:24:53 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import MySlider from "./component/rgb-panel/MySlider";
+import MyPanel from "./component/rgb-panel/MyPanel";
+import MyCalculator from "./component/calculator/MyCalculator";
+import TicTacToe from "./component/tic-tac-toe/TicTacToe";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>5b1g0011 許芸芸</h1>
+      <h2>井字遊戲</h2>
+      <TicTacToe />
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
